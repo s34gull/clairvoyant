7b0333c4518b24d5ff043784fafbd28e95187f02
s34gull <s34gull@aad51bb6-fbc2-11de-82f2-6ff60f618a82>
2010-01-07 19:43:08 +0000

    Include GPV v2 license.
    git-svn-id: http://clairvoyant.googlecode.com/svn/trunk@3 aad51bb6-fbc2-11de-82f2-6ff60f618a82

--- snapshot.sh
+++ snapshot.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 # ----------------------------------------------------------------------
-# HG Wells Time Machine
+# clairvoyant
 # ----------------------------------------------------------------------
-# This needs to be a lot more fault tolerant, but the idea is to create a 
-# rotating snapshot of the contents of $INCLUDES whenever called (minus the 
-# contents of the .exlude files). Snapshots are written to a specified sparse 
-# disk image hosted file system (via loopback) for portability. If the
-# filesystem on the disk image supports it, space is preserved via hardlinks.
+# Creates a rotating snapshot of the contents of $INCLUDES whenever 
+# called (minus the contents of the .exlude files). Snapshots are 
+# written to a specified sparse disk image hosted file system (via 
+# loopback) for portability. If the filesystem on the disk image 
+# supports it, space is preserved via hardlinks.
+# ----------------------------------------------------------------------
+# Copyright (C) 2010  Jonathan Edwards
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the 
+# Free Software Foundation, Inc., 
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 # ----------------------------------------------------------------------
 
-# suggestion from H. Milz: avoid accidental use of $PATH
 unset PATH
 
 # ----------------------------------------------------------------------
