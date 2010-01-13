f9a2848e0ae20e72ca7a3d31897191432b6ea33a
s34gull <s34gull@aad51bb6-fbc2-11de-82f2-6ff60f618a82>
2010-01-13 20:32:25 +0000

    Added license preamble; turned user and log into variables
    git-svn-id: http://clairvoyant.googlecode.com/svn/trunk@28 aad51bb6-fbc2-11de-82f2-6ff60f618a82

--- virtualbox/start_vboxheadless_wrapper.sh
+++ virtualbox/start_vboxheadless_wrapper.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
-su - vbox -c "/usr/local/bin/start_vboxheadless.sh" >> /var/log/messages 2>&1;
+# ----------------------------------------------------------------------
+# start_vboxheadless_wrapper
+# ----------------------------------------------------------------------
+# Run the start_vboxheadless.sh script as another user.
+# ----------------------------------------------------------------------
+# Copyright (C) 2010  Jonathan Edwards
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the 
+# Free Software Foundation, Inc., 
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# ----------------------------------------------------------------------
+USERAS=; # username to execute start script; must be owner and aware of all VMs in /etc/vbox/headless.config
+LOG=; # file to which ths script should append status messages
+
+su - $USERAS -c "/usr/local/bin/start_vboxheadless.sh" >> $LOG 2>&1;
 
 exit $?;
