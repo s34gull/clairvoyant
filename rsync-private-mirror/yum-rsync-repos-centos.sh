bfd13f13cf60c626cf3b077c5e02356612629d89
s34gull <s34gull@aad51bb6-fbc2-11de-82f2-6ff60f618a82>
2010-04-22 15:43:41 +0000

    Cleaned-up variables
    git-svn-id: http://clairvoyant.googlecode.com/svn/trunk@98 aad51bb6-fbc2-11de-82f2-6ff60f618a82

--- rsync-private-mirror/yum-rsync-repos-centos.sh
+++ rsync-private-mirror/yum-rsync-repos-centos.sh
@@ -5,32 +5,34 @@
 # 
 # Kickoff a clamscan of the repos
 #
-echo "`date`: Centos mirror rsync starting..." >> /var/log/messages;
+echo "`date`: Centos repo mirror: Starting..." >> /var/log/messages;
 
 cp -al /var/www/html/centos /var/www/html/.centos;
 if [ $? -ne 0 ]; then
-  echo "`date`: Centos mirror cp -al failed; exiting..." >> /var/log/messages;
+  echo "`date`: Centos repo mirror: cp -al failed; exiting..." >> /var/log/messages;
   exit 1;
 fi;
 
-for releasever in "4" "5"; 
+CENTOS_REPO_ROOT="centos.mirrors.tds.net/CentOS"
+
+for RELEASE_VER in "4" "5"; 
 do
-  for basearch in "i386" "x86_64";
+  for BASE_ARCH in "i386" "x86_64";
   do
-    if [[ ${releasever} == "4" ]] && [[ ${basearch} == "i386" ]] ; then
-      echo "`date`: Skipping CentOS ${releasever} for ${basearch} ..." >> /var/log/yum-rsync-repos-centos.log;
+    if [[ ${RELEASE_VER} == "4" ]] && [[ ${BASE_ARCH} == "i386" ]] ; then
+      echo "`date`: Centos repo mirror: Skipping CentOS ${RELEASE_VER} for ${BASE_ARCH} ..." >> /var/log/yum-rsync-repos-centos.log;
       continue;
     fi
-    for repo in addons centosplus contrib extras updates;
+    for REPO in addons centosplus contrib extras updates;
     do
-      echo "`date`: rsync --archive --verbose --partial --sparse --delete  rsync://centos.mirrors.tds.net/CentOS/${releasever}/${repo}/${basearch} \
-      --exclude=debug/ /var/www/html/.centos/${releasever}/${repo}/" >> /var/log/yum-rsync-repos-centos.log;
+      echo "`date`: rsync --archive --verbose --partial --sparse --delete  rsync://${CENTOS_REPO_ROOT}/${RELEASE_VER}/${REPO}/${BASE_ARCH} \
+      --exclude=debug/ /var/www/html/.centos/${RELEASE_VER}/${REPO}/" >> /var/log/yum-rsync-repos-centos.log;
 
-      sudo su reposync -c "rsync --archive --verbose --partial --sparse --delete  rsync://centos.mirrors.tds.net/CentOS/${releasever}/${repo}/${basearch} \
-      --exclude=debug/ /var/www/html/.centos/${releasever}/${repo}/" >> /var/log/yum-rsync-repos-centos.log 2>&1;
+      sudo su reposync -c "rsync --archive --verbose --partial --sparse --delete  rsync://${CENTOS_REPO_ROOT}/${RELEASE_VER}/${REPO}/${BASE_ARCH} \
+      --exclude=debug/ /var/www/html/.centos/${RELEASE_VER}/${REPO}/" >> /var/log/yum-rsync-repos-centos.log 2>&1;
 
       if [ $? -ne 0 ]; then
-        echo "`date`: Centos mirror [${releasever}/${repo}/${basearch}] rsync failed; continuing..." >> /var/log/messages;
+        echo "`date`: Centos repo mirror: [${CENTOS_REPO_ROOT}/${RELEASE_VER}/${REPO}/${BASE_ARCH}] rsync failed; continuing..." >> /var/log/messages;
       fi; 
     done;
   done;
@@ -40,25 +42,25 @@ done;
 #
 clamdscan --fdpass --multiscan --remove /var/www/html/.centos >> /var/log/yum-rsync-repos-centos.log 2>&1;
 if [ $? -eq 1 ]; then
-  echo "`date`: Centos mirror clamscan detected and removed infected files; contiuing..." >> /var/log/messages;
+  echo "`date`: Centos repo mirror: clamscan detected and removed infected files; contiuing..." >> /var/log/messages;
 else 
   if [ $? -eq 2 ]; then
-    echo "`date`: Centos mirror clamscan encountered an error; exiting..." >> /var/log/messages;
+    echo "`date`: Centos repo mirror: clamscan encountered an error; exiting..." >> /var/log/messages;
     exit 1;
   fi;
 fi; 
 
 rm -rf /var/www/html/centos;
 if [ $? -ne 0 ]; then
-  echo "`date`: Centos mirror rm -rf failed; exiting..." >> /var/log/messages;
+  echo "`date`: Centos repo mirror: rm -rf failed; exiting..." >> /var/log/messages;
   exit 1;
 else 
   mv /var/www/html/.centos /var/www/html/centos;
   if [ $? -ne 0 ]; then
-    echo "`date`: Centos mirror mv failed; exiting..." >> /var/log/messages;
+    echo "`date`: Centos repo mirror: mv failed; exiting..." >> /var/log/messages;
     exit 1;
   fi;
 fi;
 
-echo "`date`: Centos mirror rsync complete." >> /var/log/messages;
+echo "`date`: Centos repo mirror: Complete." >> /var/log/messages;
 exit 0;
