259241690b629c07f9b00584fd51da2508b60be1
s34gull <s34gull@aad51bb6-fbc2-11de-82f2-6ff60f618a82>
2010-04-22 16:03:35 +0000

    Includes remount to rw on start, ro on finish
    git-svn-id: http://clairvoyant.googlecode.com/svn/trunk@99 aad51bb6-fbc2-11de-82f2-6ff60f618a82

--- rsync-private-mirror/yum-rsync-repos-centos.sh
+++ rsync-private-mirror/yum-rsync-repos-centos.sh
@@ -5,34 +5,47 @@
 # 
 # Kickoff a clamscan of the repos
 #
-echo "`date`: Centos repo mirror: Starting..." >> /var/log/messages;
 
-cp -al /var/www/html/centos /var/www/html/.centos;
+LOCAL_REPO_MOUNT=/var/www/repos
+LOCAL_CENTOS_REPO=${LOCAL_REPO_MOUNT}/centos
+LOCAL_CENTOS_REPO_WORKING=${LOCAL_REPO_MOUNT}/.centos
+CENTOS_REPO_ROOT=centos.mirrors.tds.net/CentOS
+MESSAGE_LOG=/var/log/messages
+DETAIL_LOG=/var/log/yum-rsync-repos-centos.log
+
+echo "`date`: Centos repo mirror: Starting..." >> ${MESSAGE_LOG};
+
+mount -o remount,rw,noatime,nodiratime ${LOCAL_REPO_MOUNT}
 if [ $? -ne 0 ]; then
-  echo "`date`: Centos repo mirror: cp -al failed; exiting..." >> /var/log/messages;
+  echo "`date`: Centos repo mirror: Remount rw ${LOCAL_REPO_MOUNT} failed; exiting..." >> ${MESSAGE_LOG};
   exit 1;
 fi;
 
-CENTOS_REPO_ROOT="centos.mirrors.tds.net/CentOS"
+cp -al ${LOCAL_CENTOS_REPO} ${LOCAL_CENTOS_REPO_WORKING};
+if [ $? -ne 0 ]; then
+  echo "`date`: Centos repo mirror: cp -al failed; exiting..." >> ${MESSAGE_LOG};
+  exit 1;
+fi;
+
 
 for RELEASE_VER in "4" "5"; 
 do
   for BASE_ARCH in "i386" "x86_64";
   do
     if [[ ${RELEASE_VER} == "4" ]] && [[ ${BASE_ARCH} == "i386" ]] ; then
-      echo "`date`: Centos repo mirror: Skipping CentOS ${RELEASE_VER} for ${BASE_ARCH} ..." >> /var/log/yum-rsync-repos-centos.log;
+      echo "`date`: Centos repo mirror: Skipping CentOS ${RELEASE_VER} for ${BASE_ARCH} ..." >> ${DETAIL_LOG};
       continue;
     fi
     for REPO in addons centosplus contrib extras updates;
     do
       echo "`date`: rsync --archive --verbose --partial --sparse --delete  rsync://${CENTOS_REPO_ROOT}/${RELEASE_VER}/${REPO}/${BASE_ARCH} \
-      --exclude=debug/ /var/www/html/.centos/${RELEASE_VER}/${REPO}/" >> /var/log/yum-rsync-repos-centos.log;
+      --exclude=debug/ ${LOCAL_CENTOS_REPO_WORKING}/${RELEASE_VER}/${REPO}/" >> ${DETAIL_LOG};
 
       sudo su reposync -c "rsync --archive --verbose --partial --sparse --delete  rsync://${CENTOS_REPO_ROOT}/${RELEASE_VER}/${REPO}/${BASE_ARCH} \
-      --exclude=debug/ /var/www/html/.centos/${RELEASE_VER}/${REPO}/" >> /var/log/yum-rsync-repos-centos.log 2>&1;
+      --exclude=debug/ ${LOCAL_CENTOS_REPO_WORKING}/${RELEASE_VER}/${REPO}/" >> ${DETAIL_LOG} 2>&1;
 
       if [ $? -ne 0 ]; then
-        echo "`date`: Centos repo mirror: [${CENTOS_REPO_ROOT}/${RELEASE_VER}/${REPO}/${BASE_ARCH}] rsync failed; continuing..." >> /var/log/messages;
+        echo "`date`: Centos repo mirror: [${CENTOS_REPO_ROOT}/${RELEASE_VER}/${REPO}/${BASE_ARCH}] rsync failed; continuing..." >> ${MESSAGE_LOG};
       fi; 
     done;
   done;
@@ -40,27 +53,33 @@ done;
 
 # Kickoff a clamscan of the repos
 #
-clamdscan --fdpass --multiscan --remove /var/www/html/.centos >> /var/log/yum-rsync-repos-centos.log 2>&1;
+clamdscan --fdpass --multiscan --remove ${LOCAL_CENTOS_REPO_WORKING} >> ${DETAIL_LOG} 2>&1;
 if [ $? -eq 1 ]; then
-  echo "`date`: Centos repo mirror: clamscan detected and removed infected files; contiuing..." >> /var/log/messages;
+  echo "`date`: Centos repo mirror: clamscan detected and removed infected files; contiuing..." >> ${MESSAGE_LOG};
 else 
   if [ $? -eq 2 ]; then
-    echo "`date`: Centos repo mirror: clamscan encountered an error; exiting..." >> /var/log/messages;
+    echo "`date`: Centos repo mirror: clamscan encountered an error; exiting..." >> ${MESSAGE_LOG};
     exit 1;
   fi;
 fi; 
 
-rm -rf /var/www/html/centos;
+rm -rf ${LOCAL_CENTOS_REPO};
 if [ $? -ne 0 ]; then
-  echo "`date`: Centos repo mirror: rm -rf failed; exiting..." >> /var/log/messages;
+  echo "`date`: Centos repo mirror: rm -rf failed; exiting..." >> ${MESSAGE_LOG};
   exit 1;
 else 
-  mv /var/www/html/.centos /var/www/html/centos;
+  mv ${LOCAL_CENTOS_REPO_WORKING} ${LOCAL_CENTOS_REPO};
   if [ $? -ne 0 ]; then
-    echo "`date`: Centos repo mirror: mv failed; exiting..." >> /var/log/messages;
+    echo "`date`: Centos repo mirror: mv failed; exiting..." >> ${MESSAGE_LOG};
     exit 1;
   fi;
 fi;
 
-echo "`date`: Centos repo mirror: Complete." >> /var/log/messages;
+mount -o remount,ro,noatime,nodiratime ${LOCAL_REPO_MOUNT}
+if [ $? -ne 0 ]; then
+  echo "`date`: Centos repo mirror: Remount ro ${LOCAL_REPO_MOUNT} failed; exiting..." >> ${MESSAGE_LOG};
+  exit 1;
+fi;
+
+echo "`date`: Centos repo mirror: Complete." >> ${MESSAGE_LOG};
 exit 0;
